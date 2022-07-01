eb988e8a5c7b89fb5d35c11ec875a8d2ec0b1a8b
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-01 15:38:50 -0600

    Tweak for image support

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,5 @@
-function Card({ value, callback }) {
-  return (
-    <div className="card" onClick={() => callback(value)}>
-      <span>{value}</span>
-    </div>
-  );
+function Card({ imgsrc, value, callback }) {
+  return <img src={imgsrc} alt={value} onClick={() => callback(value)} />;
 }
 
 export default Card;
