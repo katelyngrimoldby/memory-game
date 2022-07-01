71d50083f217d2e3e478fdee4c01c9fc650f88ed
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-01 15:38:13 -0600

    Create image directory

---
+++ src/imgDir.js
@@ -0,0 +1,24 @@
+const IMAGES = [
+  [require('./images/alb.png'), 'alb'],
+  [require('./images/alo.png'), 'alo'],
+  [require('./images/bot.png'), 'bot'],
+  [require('./images/gas.png'), 'gas'],
+  [require('./images/ham.png'), 'ham'],
+  [require('./images/lat.png'), 'lat'],
+  [require('./images/lec.png'), 'lec'],
+  [require('./images/mag.png'), 'mag'],
+  [require('./images/msc.png'), 'msc'],
+  [require('./images/nor.png'), 'nor'],
+  [require('./images/oco.png'), 'oco'],
+  [require('./images/per.png'), 'per'],
+  [require('./images/ric.png'), 'ric'],
+  [require('./images/rus.png'), 'rus'],
+  [require('./images/sai.png'), 'sai'],
+  [require('./images/str.png'), 'str'],
+  [require('./images/tsu.png'), 'tsu'],
+  [require('./images/ver.png'), 'ver'],
+  [require('./images/vet.png'), 'vet'],
+  [require('./images/zho.png'), 'zho'],
+];
+
+export default IMAGES;
