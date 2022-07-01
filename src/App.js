00db188bd937750d1734bdfda9d4632a19e3013e
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-01 14:17:54 -0600

    Complete gameplay functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Card from './components/Card';
+import Scoreboard from './components/Scoreboard';
 
 function App() {
   const shuffle = (array) => {
@@ -22,17 +23,45 @@ function App() {
   };
 
   const [state, setState] = useState({
+    score: 0,
+    bestScore: 0,
+    prevValue: '',
     arr: ['a', 'b', 'c', 'd'],
   });
 
+  const { score, bestScore, prevValue, arr } = state;
+
   useEffect(() => {
-    setState({ arr: shuffle(state.arr) });
+    setState((prevState) => ({ ...prevState, arr: shuffle(arr) }));
   }, []);
+
+  const clickHandler = (curValue) => {
+    console.log(curValue);
+    if (curValue == prevValue) {
+      setState((prevState) => ({ ...prevState, score: 0 }));
+      setState((prevState) => ({ ...prevState, prevValue: '' }));
+    } else {
+      if (score == bestScore) {
+        setState((prevState) => ({
+          ...prevState,
+          score: score + 1,
+          bestScore: bestScore + 1,
+        }));
+      } else {
+        setState((prevState) => ({ ...prevState, score: score + 1 }));
+      }
+      setState((prevState) => ({ ...prevState, prevValue: curValue }));
+    }
+    setState((prevState) => ({ ...prevState, arr: shuffle(arr) }));
+  };
 
   return (
     <div className="App">
+      <Scoreboard score={score} bestScore={bestScore} />
       {state.arr.map((e, i) => {
-        return <Card key={i} value={e} />;
+        return (
+          <Card key={i + Date()} value={e} callback={() => clickHandler(e)} />
+        );
       })}
     </div>
   );
