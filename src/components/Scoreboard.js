751e3c6f7e81658f239f301ec41e49d8ab149144
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-03 11:41:04 -0600

    Add styling

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,11 +1,28 @@
+import styled from 'styled-components';
+
 function Scoreboard({ score, bestScore }) {
   return (
-    <div>
-      <span>
-        {score} {bestScore}
-      </span>
-    </div>
+    <ScoreboardWrapper>
+      <ScoreCounter>{score}</ScoreCounter>
+      <ScoreCounter>{bestScore}</ScoreCounter>
+    </ScoreboardWrapper>
   );
 }
+
+const ScoreboardWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-content: left;
+  align-items: center;
+  background-color: #353535;
+  padding: 10px 50px;
+`;
+
+const ScoreCounter = styled.span`
+  display: inline-block;
+  font-size: 24px;
+  font-weight: 700;
+  color: white;
+`;
 
 export default Scoreboard;
