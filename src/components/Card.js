40e355cfea43d4afc258dbac8c705f2fa9832776
katelyngrimoldby <katelyngrimoldby@gmail.com>
2022-07-03 11:40:22 -0600

    Add styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,18 @@
+import styled from 'styled-components';
 function Card({ imgsrc, value, callback }) {
-  return <img src={imgsrc} alt={value} onClick={() => callback(value)} />;
+  return (
+    <CardWrapper>
+      <img src={imgsrc} alt={value} onClick={() => callback(value)} />
+    </CardWrapper>
+  );
 }
+
+const CardWrapper = styled.div`
+  display: block;
+  border: 5px solid #353535;
+  margin-top: 10px;
+  border-radius: 15px;
+  background-color: white;
+`;
 
 export default Card;
